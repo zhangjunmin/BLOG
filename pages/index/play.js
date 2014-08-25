3fc0ba73267ca417c37f08b5fddbdd552e4b9c36
zhangjunmin <zhangjunmin123@gmail.com>
2014-08-26 01:47:24 +0800

    modify

--- pages/index/play.js
+++ pages/index/play.js
@@ -2,11 +2,14 @@
  * 播放音频
  */
 
+;(function($){
 
-;$(function(){ 
-  function play_sound(str){
+  function Audio(){
+    ready();
+  };
 
-  	function next(n){
+  Audio.prototype.play = function(str){
+    function next(n){
   		console.log(n);
   	  var index = str.indexOf(n);
   	  if(index !=str.length-1){
@@ -20,9 +23,26 @@
         audio.play();
   	  };
     }
-    next(str[0]);
+    next(str[0]);    
   };
+  function ready(){
+    var html = "<audio id ='0' src ='/sounds/0.wav'></audio>"
+              +"<audio id ='1' src ='/sounds/1.wav'></audio>"
+              +"<audio id ='2' src ='/sounds/2.wav'></audio>"
+              +"<audio id ='3' src ='/sounds/3.wav'></audio>"
+              +"<audio id ='4' src ='/sounds/4.wav'></audio>"
+              +"<audio id ='6' src ='/sounds/5.wav'></audio>"
+              +"<audio id ='7' src ='/sounds/7.wav'></audio>"
+              +"<audio id ='8' src ='/sounds/8.wav'></audio>"
+              +"<audio id ='9' src ='/sounds/9.wav'></audio>";
+    $('body').append(html);       
+  }; 
+  window.Audio = Audio;
+})(jQuery);
+
+$(function(){
+  var audio = new Audio();
   $('#play').click(function(){
-    play_sound('3572');
+    audio.play('3572');
   });	
 });
